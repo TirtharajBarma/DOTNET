c6049ac523d20942665f6198e3fbfc488c812a71
TIRTHARAJ BARMA <119959056+TirtharajBarma@users.noreply.github.com>
2026-01-24 09:37:43 +0530

    Saturday assignment

---
+++ SaturdayAssigment/3rd_app/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using reflection;
+
+// Demonstrating explicit interface implementation
+
+class Program
+{
+    public static void Main()
+    {
+        // Class1 obj = new Class1();
+        // I1 interface1 = obj;
+        // interface1.M1();
+        // interface1.M2();
+
+        // I2 interface2 = obj;
+        // interface2.M1();
+        // interface2.M2();
+
+        Assembly asm = Assembly.Load("reflection");
+        foreach (Type type in asm.GetTypes())
+        {
+            // Console.WriteLine($"Type: {type.Name}");
+            if(type.IsInterface)
+                Console.WriteLine($"Interface Name: {type.Name}");
+
+            if(type.IsClass)
+                Console.WriteLine($"Class Name: {type.Name}");
+            
+            MethodInfo[] methods = type.GetMethods(
+                BindingFlags.Instance |
+                BindingFlags.Public |
+                BindingFlags.NonPublic 
+            );
+            foreach(var method in methods)
+                Console.WriteLine($"methods name: {method}");
+        }
+    }
+}
