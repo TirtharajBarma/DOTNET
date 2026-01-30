3a1898eb0705429124f1d27faf7e5951cdac79b8
TIRTHARAJ BARMA <119959056+TirtharajBarma@users.noreply.github.com>
2026-01-30 10:16:33 +0530

    extra pratice

---
+++ 3rd_app/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using reflection;
+using CustomerSorting;
+
+// Demonstrating explicit interface implementation
+
+class Program
+{
+    public static void Main()
+    {
+        // Class1 obj = new Class1();
+        // I1 interface1 = obj;
+        // interface1.M1();
+        // interface1.M2();
+
+        // I2 interface2 = obj;
+        // interface2.M1();
+        // interface2.M2();
+
+        // Assembly asm = Assembly.Load("reflection");
+        // foreach (Type type in asm.GetTypes())
+        // {
+        //     // Console.WriteLine($"Type: {type.Name}");
+        //     if(type.IsInterface)
+        //         Console.WriteLine($"Interface Name: {type.Name}");
+
+        //     if(type.IsClass)
+        //         Console.WriteLine($"Class Name: {type.Name}");
+            
+        //     MethodInfo[] methods = type.GetMethods(
+        //         BindingFlags.Instance |
+        //         BindingFlags.Public |
+        //         BindingFlags.NonPublic 
+        //     );
+        //     foreach(var method in methods)
+        //         Console.WriteLine($"methods name: {method}");
+        // }
+
+        //! Day22
+        CustomerSorting.Program.main();
+    }
+}
