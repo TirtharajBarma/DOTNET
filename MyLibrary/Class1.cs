26ae2e0c0123488fb216969147aec3f54bf4fe77
TIRTHARAJ BARMA <119959056+TirtharajBarma@users.noreply.github.com>
2026-01-30 09:44:36 +0530

    DLL

---
+++ MyLibrary/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace CalculatorDLL;
+
+public class Calculator
+{
+    public int add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int sub(int a, int b)
+    {
+        return a - b;
+    }
+}
---
+++ reflection/Class1.cs
@@ -0,0 +1,53 @@
+﻿namespace reflection;
+
+public interface I1{
+    public void M1();
+    public void M2();
+}
+
+public interface I2
+{
+    public void M1();
+    public void M2();
+}
+
+public interface I3{
+    public void M1();
+    public void M2();
+}
+
+public class Class1 : I1, I2, I3
+{
+    void I1.M1()
+    {
+        Console.WriteLine("M1 methods");
+    }
+    void I1.M2()
+    {
+        Console.WriteLine("M2 methods");
+    }
+    void I2.M1()
+    {
+        Console.WriteLine("M1 methods");
+    }
+    void I2.M2()
+    {
+        Console.WriteLine("M2 methods");
+    }
+    void I3.M1()
+    {
+        Console.WriteLine("M1 methods");
+    }
+    void I3.M2()
+    {
+        Console.WriteLine("M2 methods");
+    }
+
+}
+
+public class Class2 : Class1{
+    public void Class2Method()
+    {
+        Console.WriteLine("class 2 method");
+    }
+}
